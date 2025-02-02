1249347eb3f996a6dddbdb643f2f423b466d1c0a
Meghashyam12 <86647274+Meghashyam12@users.noreply.github.com>
2025-02-02 21:13:25 +0530

    part one complete

--- day_01.js
+++ day_01.js
@@ -1,5 +1,5 @@
-const part_1 = (input) => {
-  console.log(input);
+const part_1 = (modules) => {
+  return modules.reduce((fuel, module) => fuel + Math.floor(module / 3) - 2, 0);
 };
 
 // const part_2 = (input) => {};
@@ -7,7 +7,7 @@ const part_1 = (input) => {
 const main = () => {
   const file = Deno.readTextFileSync("./inputs/day_01.txt");
   const input = file.split("\n");
-  part_1(input);
+  console.log(part_1(input));
   // part_2(input);
 };
 
