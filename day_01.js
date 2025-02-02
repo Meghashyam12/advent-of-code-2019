653b2684a7a141846370523658937155b37ffe38
Meghashyam12 <86647274+Meghashyam12@users.noreply.github.com>
2025-02-02 21:53:58 +0530

    part two complete

--- day_01.js
+++ day_01.js
@@ -2,13 +2,26 @@ const part_1 = (modules) => {
   return modules.reduce((fuel, module) => fuel + Math.floor(module / 3) - 2, 0);
 };
 
-// const part_2 = (input) => {};
+const calculateRequiredFuel = (fuel, module) => {
+  const requiredFuel = Math.floor(module / 3) - 2;
+
+  if (requiredFuel <= 0) {
+    return fuel;
+  }
+
+  return requiredFuel + calculateRequiredFuel(fuel, requiredFuel);
+};
+
+const part_2 = (modules) => {
+  return modules.reduce(calculateRequiredFuel, 0);
+};
 
 const main = () => {
   const file = Deno.readTextFileSync("./inputs/day_01.txt");
   const input = file.split("\n");
+
   console.log(part_1(input));
-  // part_2(input);
+  console.log(part_2(input));
 };
 
 main();
