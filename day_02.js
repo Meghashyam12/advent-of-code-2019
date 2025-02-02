1a2fdbcbb5f7ea6d6c90ce131b44c9b1d6b42c72
Meghashyam12 <86647274+Meghashyam12@users.noreply.github.com>
2025-02-02 22:44:18 +0530

    completed part two

--- day_02.js
+++ day_02.js
@@ -1,5 +1,7 @@
-const part_1 = (input) => {
+const part_1 = (noun, verb, input) => {
   const opcode = [...input];
+  opcode[1] = noun;
+  opcode[2] = verb;
   let i = 0;
 
   while (i < opcode.length) {
@@ -25,16 +27,22 @@ const part_1 = (input) => {
   }
 };
 
-const part_2 = (input) => {};
+const part_2 = (input) => {
+  for (let noun = 0; noun < 100; noun++) {
+    for (let verb = 0; verb < 100; verb++) {
+      if (part_1(noun, verb, input) === 19690720) {
+        return 100 * noun + verb;
+      }
+    }
+  }
+};
 
 const main = () => {
   const file = Deno.readTextFileSync("./inputs/day_02.txt");
   const input = file.split(",").map((num) => +num);
 
-  input[1] = 12;
-  input[2] = 2;
-  console.log(part_1(input));
-  // console.log(part_2(input));
+  console.log(part_1(12, 2, input));
+  console.log(part_2(input));
 };
 
 main();
